f8ecf1cc723af044ba4e5842755ab5389923cc97
AntonyCoder <69346513+AntonyCoder@users.noreply.github.com>
2020-11-16 23:37:24 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -47,14 +47,3 @@ jQuery(function ($) {
     }
   });
 });
-
-//модальное окно
-var elements = $('.modal-overlay, .modal ');
-
-$('button').click(function () {
-  elements.addClass('active');
-});
-
-$('.close-modal').click(function () {
-  elements.removeClass('active');
-});
